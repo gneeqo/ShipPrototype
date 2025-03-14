89a7dd6f89ef90f7e03bb7f2f4973faba041760b
gneeqo <nicolastoe@gmail.com>
2025-03-13 21:51:25 -0700

    jank parallax

---
+++ Game Logic/ship_tracking_camera.gd
@@ -0,0 +1,17 @@
+extends Camera2D
+
+@export var ship : Ship
+@export var zoom_factor : float
+@export var zoom_max : float
+func _process(delta:float):
+    var ship_speed = \
+    PhysicsServer2D.body_get_direct_state(ship.get_rid()).linear_velocity.length()
+    var zoom_amount:float = 1/((1+(ship_speed/zoom_factor)))
+    
+    if zoom_amount < zoom_max:
+        zoom = Vector2(zoom_max,zoom_max)
+    else:
+        zoom = Vector2(zoom_amount,zoom_amount)
+    
+    position = ship.position
+    
